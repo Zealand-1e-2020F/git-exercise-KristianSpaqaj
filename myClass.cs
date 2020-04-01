c9738ca201600b675b567bdf29d6b9606a193130
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-04-01 10:57:42 +0200

    Added Constructor
    tilføjet en Constructor til myClass

--- myClass.cs
+++ myClass.cs
@@ -6,9 +6,17 @@ namespace GitExercise
         private string _addresse;
         private int _postNr;
 
+        public myClass(string Navn, string Addresse, int PostNr)
+        {
+            Navn = _navn;
+            Addresse = _addresse;
+            PostNr = _postNr;
+        }
 
         public int PostNr { get => _postNr; set => _postNr = value; }
         public string Addresse { get => _addresse; set => _addresse = value; }
         public string Navn { get => _navn; set => _navn = value; }
+
+        
     }
 }
