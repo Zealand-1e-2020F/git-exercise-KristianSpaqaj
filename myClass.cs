772a815d898741cb2bfab60d8d59d75d620b2ce8
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-04-01 10:49:57 +0200

    Added properties
    Tilføjet 3 properties (navn,addresse,postnr)

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,13 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        private string _navn;
+        private string _addresse;
+        private int _postNr;
+
+
+        public int PostNr { get => _postNr; set => _postNr = value; }
+        public string Addresse { get => _addresse; set => _addresse = value; }
+        public string Navn { get => _navn; set => _navn = value; }
     }
 }
